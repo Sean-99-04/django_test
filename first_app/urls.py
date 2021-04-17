5b056e2a37852795c2e8f4df8e5ddb9a20cb1386
Sean Stocker <s.stocks99@gmail.com>
2021-04-17 20:49:03 +0100

    Added create form

--- first_app/urls.py
+++ first_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('create-view/', views.ObjectCreate.as_view(), name='create-view')
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+
+from django.urls import reverse_lazy
 
 from .models import MyObject
 
@@ -9,3 +12,10 @@ class IndexView(ListView):
     model = MyObject
     context_object_name = 'objects'
     template_name = 'first_app/index.html'
+
+class ObjectCreate(CreateView):
+    model = MyObject
+    fields = ['title', 'description']
+    template_name = 'first_app/form.html'
+    success_url = reverse_lazy('index')
+    # form_class = MyObject
